6ae86d2479a95795aecf2bd135822790f5d3799e
rahul ray <rahul@navigus.in>
2022-01-29 12:54:28 +0530

    feat: Added ping route.

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/ping", (req, res) => {
+  res.status(200).send("Pong");
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
